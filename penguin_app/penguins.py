9cdfddab8d23b2688571a51ab1c25a87bd3a9f82
AkshatKumar99 <kumar.aks99@gmail.com>
2023-11-27 15:28:18 -0600

    finished penguin_app

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -2,9 +2,23 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import seaborn as sns
+import time
+
+@st.cache_data()
+def load_file(penguin_file):
+    time.sleep(3)
+    if penguin_file is not None:
+        df = pd.read_csv(penguin_file)
+    else:
+        df = pd.read_csv('penguins.csv')
+    return df
 
 st.title("Palmer's Penguins")
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
+
+#penguins_subset = penguins_df[penguins_df['species'] == selected_species]
+penguin_file = st.file_uploader('Select Your Local Penguins CSV(default provided)')
+penguins_df = load_file(penguin_file)
 
 selected_x_var = st.selectbox('What do you want the x variable to be?',
                               ['bill_length_mm', 'bill_depth_mm', 
@@ -12,15 +26,15 @@ selected_x_var = st.selectbox('What do you want the x variable to be?',
 selected_y_var = st.selectbox('What about the y?',
                               ['bill_length_mm', 'bill_depth_mm',
                                'flipper_length_mm', 'body_mass_g'])
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                               ['all_penguins', 'male penguins', 'female penguins'])
 
-#penguins_subset = penguins_df[penguins_df['species'] == selected_species]
-penguin_file = st.file_uploader('Select Your Local Penguins CSV(default provided)')
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
+if selected_gender == 'male penguins':
+    penguins_df = penguins_df[penguins_df.sex == 'male']
+elif selected_gender == 'female penguins':
+    penguins_df = penguins_df[penguins_df.sex == 'female']
 else:
-    st.stop()
-sns.set_style('darkgrid')
-markers = {"Adelie": "X", "Gentoo": "s", "Chinstrap": 'o'}
+    pass
 
 alt_chart = (
     alt.Chart(penguins_df, title=f'Scatterplot of Palmer\'s Penguins')
