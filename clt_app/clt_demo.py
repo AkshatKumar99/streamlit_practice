98e9e2ea3782800295bd7eeeb8a3363ae06dce38
AkshatKumar99 <kumar.aks99@gmail.com>
2023-11-27 13:48:32 -0600

    added graph to CLT_app

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -1,6 +1,25 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
+
+MIN_VAL = 1
+MAX_VAL = 5000
+num = 100
+
+st.write('# Central Limit Theorem App')
 
 binom_dist = np.random.binomial(1, .5, 100)
-st.write(np.mean(binom_dist))
- 
+list_of_means = []
+
+for i in range(0,1000):
+    list_of_means.append(np.random.choice(binom_dist, num, replace=True).mean())
+    
+fig, ax = plt.subplots()
+
+ax = plt.hist(list_of_means)
+
+st.pyplot(fig)
+
+num = st.slider("Sample Size:", min_value=MIN_VAL, max_value=MAX_VAL, value=100)
+
+st.button('Re-run')
